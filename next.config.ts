9e687f102d1d172c9f0166ebad942f57e8305259
ryan-lester <ryansplanning@gmail.com>
2026-03-12 00:05:20 -0400

    Removed GitHub pages config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    // Empty config! Vercel handles all the magic for you automatically.
 };
 
 export default nextConfig;
